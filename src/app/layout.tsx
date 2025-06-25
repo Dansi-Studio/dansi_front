065f4ab3a56033fda9d351fde65fc4cb9ed118a6
youngchannel <youngchannel4u@gmail.com>
2025-06-26 01:20:16 +0900

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,6 +44,10 @@ export const viewport: Viewport = {
   themeColor: "#735030",
   width: "device-width",
   initialScale: 1,
+  maximumScale: 1,
+  minimumScale: 1,
+  userScalable: false,
+  viewportFit: "cover",
 };
 
 export default function RootLayout({
